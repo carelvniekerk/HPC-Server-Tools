afb6417afbb3cae5ad00a81ae42e6b1c54ee0b0a
Carel van Niekerk <cvanniekerk@hhu.de>
2024-08-19 09:47:07 +0200

    Refactor tmux_dev_session.sh to dynamically resize panes based on window width

--- tools/tmux_dev_session.sh
+++ tools/tmux_dev_session.sh
@@ -14,7 +14,7 @@ tmux split-window -h
 
 # Resize the panes
 tmux select-pane -t 1
-tmux resize-pane -R 80
+tmux resize-pane -x $(echo "$(tmux display -p "#{window_width}")*0.8/1" | bc)
 
 # Run commands in each pane
 tmux select-pane -t 0
