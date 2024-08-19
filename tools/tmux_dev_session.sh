a1926981848f3302a9f20fa405b4af0172c5b9c3
Carel van Niekerk <cvanniekerk@hhu.de>
2024-08-19 15:03:45 +0200

    Refactor tmux_dev_session.sh to resize the first pane by 85% of the window width

--- tools/tmux_dev_session.sh
+++ tools/tmux_dev_session.sh
@@ -14,7 +14,25 @@ tmux split-window -h
 
 # Resize the panes
 tmux select-pane -t 0
-tmux resize-pane -x $(echo "$(tmux display -p "#{window_width}")*0.85/1" | bc)
+# Define the minimum width required for the right pane
+min_right_pane_width=25
+
+# Get the total width of the terminal
+total_width=$(tmux display -p "#{window_width}")
+
+# Calculate the target width for pane 0 (85% of total width)
+target_left_pane_width=$(echo "$total_width*0.85/1" | bc)
+
+# Calculate the remaining width for the right pane
+right_pane_width=$(($total_width - $target_left_pane_width))
+
+# If the right pane is too small, adjust the left pane width
+if [ $right_pane_width -lt $min_right_pane_width ]; then
+  target_left_pane_width=$(($total_width - $min_right_pane_width))
+fi
+
+# Resize pane 0 to the calculated width
+tmux resize-pane -x $target_left_pane_width
 
 # Run commands in each pane
 tmux select-pane -t 0
